3a1bcbd73a6dc185ba9cab9d391bff697dd31c76
Andrew <93141508+svpra@users.noreply.github.com>
2023-06-29 02:20:01 +0300

    Add files via upload

---
+++ Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Web.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public ApplicationUser Admin { get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}
